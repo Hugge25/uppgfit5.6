28a68a0b9dfb6add5fe6ace2c94bc165041a911a
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-27 10:51:36 +0100

    Add project files.

---
+++ sheesh/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett visst antal tal gånger varanda. Exempel: 5*3*8 \n");
+            string text = Console.ReadLine();
+
+            string[] tal = text.Split("*");
+            int[] intTal = Array.ConvertAll(tal, int.Parse);
+
+            int store = 1;
+
+            for(int i = 0; i < intTal.Length; i++)
+            {
+                store = intTal[i] * store;
+            }
+
+            Console.Write(store);
+        }
+    }
+}
